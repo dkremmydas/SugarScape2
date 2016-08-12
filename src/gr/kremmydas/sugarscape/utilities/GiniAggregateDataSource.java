e8bcbd10c3549cd9da6cc10bf2c5111da8dd07e1
Dimitria Kremmydas - ASUS laptop <kremmydas@aua.gr>
2016-08-12 17:50:09 +0300

    Gini calculated and related time series

---
+++ src/gr/kremmydas/sugarscape/utilities/GiniAggregateDataSource.java
@@ -0,0 +1,81 @@
+package gr.kremmydas.sugarscape.utilities;
+
+import java.util.Arrays;
+
+import gr.kremmydas.sugarscape.agents.AgentChapter2_p30;
+import repast.simphony.data2.AggregateDataSource;
+
+public class GiniAggregateDataSource implements AggregateDataSource {
+	
+	private String id = "GiniCoefficient";
+
+	public GiniAggregateDataSource() {
+		
+	}
+
+	@Override
+	public String getId() {
+		return id;
+	}
+
+	@Override
+	public Class<?> getDataType() {
+		return Double.class;
+	}
+
+	@Override
+	public Class<?> getSourceType() {
+		return AgentChapter2_p30.class;
+	}
+
+	@Override
+	public Object get(Iterable<?> objs, int size) {
+		double[] values = new double[size];
+		double r = 0d;
+		
+		//collect sugar wealth for every agent
+		int i=0;
+		for(Object o: objs) {
+			AgentChapter2_p30 a = (AgentChapter2_p30) o;
+			values[i++] = a.getSugarWealth();			
+		}
+		
+		//sort values
+		Arrays.sort(values);
+		
+		r = calcGini(values, size);			
+		
+		return r;
+	}
+
+	@Override
+	public void reset() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private double calcGini(double[] vs, int count) {
+		double ginisum = 0;
+		double sum = 0;
+		long i = 1;
+		
+		// auxiliaries
+		long skip = 0;
+		
+		for ( int y=0;y<count;y++ )
+		{
+			double lElem = vs[y];
+			
+			long off = 2 * i - count - skip - 1;
+			ginisum = ginisum + off * lElem;
+			sum += lElem;		
+			i++;
+		}
+		
+		if ( sum != 0 )
+			return ginisum / (double)(count - skip) / sum;
+		else
+			return Double.NaN; // no occurrence recorded
+	}
+
+}
