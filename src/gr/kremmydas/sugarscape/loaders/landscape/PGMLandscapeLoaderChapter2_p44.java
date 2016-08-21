76d3d1d35f349df850ca3c50a413b0343527722b
Dimitria Kremmydas - ASUS laptop <kremmydas@aua.gr>
2016-08-21 16:13:35 +0300

    ch2-p44 works

--- src/gr/kremmydas/sugarscape/loaders/landscape/PGMLandscapeLoaderChapter2_p44.java
+++ src/gr/kremmydas/sugarscape/loaders/landscape/PGMLandscapeLoaderChapter2_p44.java
@@ -2,13 +2,9 @@ package gr.kremmydas.sugarscape.loaders.landscape;
 
 
 
-import gr.kremmydas.sugarscape.agents.Agent;
 import gr.kremmydas.sugarscape.landscape.Landscape;
 import gr.kremmydas.sugarscape.landscape.LandscapeChapter2_p30;
-import gr.kremmydas.sugarscape.landscape.rules.growback.GrowbackAbility;
 import gr.kremmydas.sugarscape.landscape.rules.growback.GrowbackRule_p44;
-import repast.simphony.engine.environment.RunEnvironment;
-import repast.simphony.space.grid.RandomGridAdder;
 
 /**
  * <p>Set the season to be summer and initialize the regenerationRate accordingly</p>
@@ -20,30 +16,12 @@ public class PGMLandscapeLoaderChapter2_p44 extends PGMLandscapeLoaderChapter2_p
 	
 	@Override
 	public Landscape load() {
-		LandscapeChapter2_p30 ls  = new LandscapeChapter2_p30(pgmreader.getxSize(), pgmreader.getySize());
+		LandscapeChapter2_p30 ls  = (LandscapeChapter2_p30) super.load();
 		
-		GrowbackAbility ga; GrowbackRule_p44 ga44;
-		try {
-			ga = (GrowbackAbility) Class.forName(RunEnvironment.getInstance().getParameters().getString("growbackRuleClass")).newInstance();
-			ls.setGrowbackRule(ga);
-			
-			ga44 = (GrowbackRule_p44) ga;
-			GrowbackRule_p44.setSeasonSummer();
-			ga44.setRegenerationRate(ls);
-			
-			
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-			RunEnvironment.getInstance().endRun();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
-			e.printStackTrace();
-			RunEnvironment.getInstance().endRun();
-		}
-		
-		super.setInitialSugarQuantity(ls);
-		ls.getGrid().setAdder(new RandomGridAdder<Agent>());
+		GrowbackRule_p44 ga44 = (GrowbackRule_p44) ls.getGrowbackRule();
+		GrowbackRule_p44.setSeasonSummer();
+		ga44.setRegenerationRate(ls);
+	
 		
 		return ls;
 	}
